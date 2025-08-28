986bb1f204e9d0c87b5878fde77452bb41f8b044
jantusaf <jantusaf@gmail.com>
2025-08-27 23:05:55 -0300

    #esqueleto menu

--- Gestor Gimnasio/DashboardAdmin.Designer.cs
+++ Gestor Gimnasio/DashboardAdmin.Designer.cs
@@ -28,33 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cLIENTEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vERToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aÑADIRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // panel1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 33);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.ForeColor = System.Drawing.Color.Navy;
+            this.panel1.Location = new System.Drawing.Point(3, -6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(990, 103);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = global::Gestor_Gimnasio.Properties.Resources.logo;
+            this.pictureBox1.Image = global::Gestor_Gimnasio.Properties.Resources.logo;
+            this.pictureBox1.InitialImage = global::Gestor_Gimnasio.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(144, 103);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel2.Controls.Add(this.menuStrip1);
+            this.panel2.Location = new System.Drawing.Point(3, 103);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(190, 511);
+            this.panel2.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cLIENTEToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(190, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cLIENTEToolStripMenuItem
+            // 
+            this.cLIENTEToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vERToolStripMenuItem,
+            this.aÑADIRToolStripMenuItem});
+            this.cLIENTEToolStripMenuItem.Name = "cLIENTEToolStripMenuItem";
+            this.cLIENTEToolStripMenuItem.Size = new System.Drawing.Size(78, 24);
+            this.cLIENTEToolStripMenuItem.Text = "CLIENTE";
+            // 
+            // vERToolStripMenuItem
+            // 
+            this.vERToolStripMenuItem.Name = "vERToolStripMenuItem";
+            this.vERToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.vERToolStripMenuItem.Text = "VER";
+            // 
+            // aÑADIRToolStripMenuItem
+            // 
+            this.aÑADIRToolStripMenuItem.Name = "aÑADIRToolStripMenuItem";
+            this.aÑADIRToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.aÑADIRToolStripMenuItem.Text = "AÑADIR";
             // 
             // DashboardAdmin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(989, 592);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.DarkRed;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "DashboardAdmin";
             this.Text = "DashboardAdmin";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cLIENTEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vERToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aÑADIRToolStripMenuItem;
     }
 }
--- Gestor Gimnasio/DashboardAdmin.cs
+++ Gestor Gimnasio/DashboardAdmin.cs
@@ -16,5 +16,7 @@ namespace Gestor_Gimnasio
         {
             InitializeComponent();
         }
+
+        
     }
 }
